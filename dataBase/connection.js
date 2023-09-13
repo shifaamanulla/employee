7a3c8abdf6df2628e4d6fb767944cfe8614e810d
unknown <shifaamanulla123@gmail.com>
2023-09-13 11:41:40 +0530

    message

--- dataBase/connection.js
+++ dataBase/connection.js
@@ -1,12 +1,22 @@
+// const mongoose=require('mongoose')
+// mongoose.connect(process.env.baseUrl,{
+//     //unexpected connection error can be solved using this 
+//     useNewUrlParser:true,
+//     useUnifiedTopology:true
+//     //then is used to get resolved output
+// }).then(()=>{
+//     console.log("_______________Mongodb Atlast connected____________");
+// }).catch((err)=>{
+//     console.log(`-------------Mongodb connection error---------------${err}`);
+// })
+
 const mongoose=require('mongoose')
+
 mongoose.connect(process.env.baseUrl,{
-    //unexpected connection error can be solved using this 
     useNewUrlParser:true,
     useUnifiedTopology:true
-    //then is used to get resolved output
 }).then(()=>{
-    console.log("_______________Mongodb Atlast connected____________");
+    console.log("________Mongodb Atlas Connected_________");
 }).catch((err)=>{
-    console.log(`-------------Mongodb connection error---------------${err}`);
+    console.log(`........mogodb connection error.........${err}`);
 })
-
